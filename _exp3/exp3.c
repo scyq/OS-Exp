4661e54c84e2b7d38eae809f103321dbd9a38c96
scyq <chenyuqing0429@foxmail.com>
2020-11-10 11:25:16 +0800

    exp3 finished

---
+++ _exp3/exp3.c
@@ -0,0 +1,47 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void *char_thread(void *arg);
+void *int_thread(void *arg);
+
+int main()
+{
+    /*
+        tid_0 thread 0 char_thread
+        tid_1 thread 1 int_thread
+    */
+    pthread_t tid_0, tid_1;
+    int re_code0, re_code1;
+    char c = 'a';
+    int i = 10;
+
+    re_code0 = pthread_create(&tid_0, NULL, char_thread, &c);
+    re_code1 = pthread_create(&tid_1, NULL, int_thread, &i);
+
+    if (re_code0 || re_code1)
+    {
+        printf("Thread Create Err");
+        exit(1);
+    }
+    else
+    {
+        pthread_join(tid_0, NULL);
+        pthread_join(tid_1, NULL);
+    }
+
+    return 0;
+}
+
+void *char_thread(void *arg)
+{
+    char *char_pointer = arg;
+    printf("Char is %c\n", *char_pointer);
+    pthread_exit(0);
+}
+
+void *int_thread(void *arg)
+{
+    int *int_pointer = arg;
+    printf("Int is %d\n", *int_pointer);
+    pthread_exit(0);
+}
---
+++ _exp3/exp3_extra.c
@@ -0,0 +1,111 @@
+#include <pthread.h>
+#include <stdio.h>
+
+/* quick sort */
+void swap(int *a, int *b)
+{
+    int t = *b;
+    *b = *a;
+    *a = t;
+}
+
+void div(int arr[], int start, int end)
+{
+    if (start >= end)
+        return;
+    int pivot = arr[end];
+    int left = start;
+    int right = end - 1;
+    while (left < right)
+    {
+        while (left < right && arr[left] < pivot)
+            left++;
+        while (left < right && arr[right] > pivot)
+            right--;
+        swap(&arr[left], &arr[right]);
+    }
+    if (arr[left] >= arr[end])
+        swap(&arr[left], &arr[end]);
+    else
+        left++;
+    if (left)
+        div(arr, start, left - 1);
+    div(arr, left + 1, end);
+}
+
+void sort(int arr[], int len)
+{
+    div(arr, 0, len - 1);
+}
+
+void sort_reverse(int arr[], int len)
+{
+    div(arr, 0, len - 1);
+    int temp[len];
+    for (int i = 0; i < len; i++)
+        temp[len - i - 1] = arr[i];
+    for (int i = 0; i < len; i++)
+        arr[i] = temp[i];
+}
+
+/* ------------------------------------------------------------ */
+/* main process */
+
+void *sort_thread(void *arg);
+void *re_sort_thread(void *arg);
+
+int main()
+{
+    int arr[10] = {1, 4, 7, 9, 0, 3, 2, 5, 6, 8};
+
+    pthread_t tid_0, tid_1;
+    int re_code0, re_code1;
+
+    re_code0 = pthread_create(&tid_0, NULL, sort_thread, arr);
+    if (re_code0)
+    {
+        printf("Thread Create Err");
+        exit(0);
+    }
+    else
+    {
+        pthread_join(tid_0, NULL); /* wait thread 0 finished */
+    }
+    re_code1 = pthread_create(&tid_1, NULL, re_sort_thread, arr);
+    if (re_code1)
+    {
+        printf("Thread Create Err");
+        exit(0);
+    }
+    else
+    {
+        pthread_join(tid_1, NULL);
+        printf("Two threads executed.\n");
+    }
+}
+
+void *sort_thread(void *arg)
+{
+    int *arr = arg;
+    sort(arr, 10);
+    printf("Small to Big: ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    pthread_exit(0);
+}
+
+void *re_sort_thread(void *arg)
+{
+    int *arr = arg;
+    sort_reverse(arr, 10);
+    printf("Big to Small: ");
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    pthread_exit(0);
+}
